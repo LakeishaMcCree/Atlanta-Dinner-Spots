d3eb99b5556496c58f78592e107e68fdc106b403
Lakeisha McCree <lakeisha.mccree215@gmail.com>
2020-03-15 18:45:46 +0000

    first commit

---
+++ AtlantaDinnerSpots/lib/AtlantaDinnerSpots.rb
@@ -0,0 +1,6 @@
+require "AtlantaDinnerSpots/version"
+
+module AtlantaDinnerSpots
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ AtlantaDinnerSpots/lib/AtlantaDinnerSpots/version.rb
@@ -0,0 +1,3 @@
+module AtlantaDinnerSpots
+  VERSION = "0.1.0"
+end
